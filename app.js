1d69fb8cf003cd74f98766d0a27039e473ed1adc
panchosoft <panchohk@gmail.com>
2020-11-29 00:02:23 -0500

    Route 53 record name changes based on request's query parameters

--- app.js
+++ app.js
@@ -5,60 +5,50 @@ var Route53Client = require('aws-sdk/clients/route53');
 var route53 = new Route53Client();
 
 module.exports.update = (event, context, callback) => {
-  // Required params
+  // Required params (errors written to CloudWatch logs)
   if (!event.queryStringParameters.hosted_zone_id) { callback('Missing hosted_zone_id parameter'); return false; }
-  if (!event.queryStringParameters.name) { callback('Missing name parameter'); return false; }
-  if(!event.requestContext.identity.sourceIp){  callback('Missing IP parameter'); return false; }
+  if (!event.queryStringParameters.record_name) { callback('Missing name parameter'); return false; }
+  if (!event.requestContext.identity.sourceIp) { callback('Missing IP parameter'); return false; }
 
   // Success response
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello, the current time is ${new Date().toTimeString()}.`,
-      event: event,
-      context: context
+      message: `Success. Execution time: ${new Date().toTimeString()}.`,
+      // event: event,
+      // context: context
     }),
   };
 
-  /* The following example creates a resource record set that routes Internet traffic to a resource with an IP address of 192.0.2.44. */
-
+  // Route 53 client request params
   var params = {
     ChangeBatch: {
       Changes: [
         {
-          Action: "UPSERT",
+          Action: "UPSERT", // Create or Update Route 53 record name
           ResourceRecordSet: {
-            Name: event.queryStringParameters.name,
+            Name: event.queryStringParameters.record_name, // Record name
             ResourceRecords: [
               {
-                Value: event.requestContext.identity.sourceIp
+                Value: event.requestContext.identity.sourceIp // Request's IP address
               }
             ],
-            TTL: 300,
-            Type: "A"
+            TTL: 300, // Time to live
+            Type: "A" // Record Type
           }
         }
-      ],
-      Comment: "Web server for example.com"
+      ]
     },
-    HostedZoneId: event.queryStringParameters.hosted_zone_id
+    HostedZoneId: event.queryStringParameters.hosted_zone_id // Route 53 Hosted Zone Id
   };
-  route53.changeResourceRecordSets(params, function (err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else console.log(data);           // successful response
-    /*
-    data = {
-     ChangeInfo: {
-      Comment: "Web server for example.com", 
-      Id: "/change/C2682N5HXP0BZ4", 
-      Status: "PENDING", 
-      SubmittedAt: <Date Representation>
-     }
-    }
-    */
 
+  // Create/Update Route 53 record name based on the specified hosted zone id and record name
+  route53.changeResourceRecordSets(params, function (err, data) {
+    // Write result to CloudWatch logs
+    if (err) console.log(err, err.stack); // An error occurred
+    else console.log(data);           // Successful response
+
+    // Return response
     callback(null, response);
   });
-
-
 };
