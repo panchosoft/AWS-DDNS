4cf22f836aff5e83e06802ae22923f9a8fb0674e
panchosoft <panchohk@gmail.com>
2020-11-28 21:57:29 -0500

    Updated serverless.yml file to run serverless deploy command.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports.endpoint = (event, context, callback) => {
+module.exports.update = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
